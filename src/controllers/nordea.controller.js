115bbf9211c836bc12965b2cac6ec92658990116
Oskar Yildiz <yildizoskar@gmail.com>
2017-11-25 19:16:31 +0200

    added nordeaAuth routes and contrroller

---
+++ src/controllers/nordea.controller.js
@@ -0,0 +1,30 @@
+import Joi from 'joi';
+
+import constants from '../config/constants';
+import { filteredBody } from '../utils/filteredBody';
+import { getAccessToken } from '../services/nordea-api';
+import User from '../models/user.model';
+
+export const validation = {
+  nordeaAuth: {
+    body: {
+      accessCode: Joi.string().required(),
+    },
+  },
+};
+
+export const nordeaAuth = async (req, res, next) => {
+  try {
+    const data = filteredBody(req.body, constants.WHITELIST.nordea.auth);
+    const accessToken = getAccessToken(data.accessCode);
+
+    await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { $set: { nordea: { accessToken } } },
+    );
+
+    return res.status(200).json();
+  } catch (err) {
+    next(err);
+  }
+};
