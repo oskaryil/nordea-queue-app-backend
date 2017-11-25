f55fae6ce54ea71e2f438c83f3ed7360918aed67
Anton Myrberg <15anmy@stockholmscience.se>
2017-11-25 13:29:02 +0100

    Started working on the getAccounts function which gets sample data from nordea-api service

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,11 +1,19 @@
+import * as account from '../services/account-info';
+import User from '../models/user.model';
 /*
   TODO:
     Create getAccount, getAccountById.... using account-info.js service
  */
-const getAccount = (req, res) => {
-  res.status(200).send('account');
+const getAccounts = async (req, res, next) => {
+  try {
+    const accounts = await account.fetchAccounts();
+    const userId = req.user._id;
+
+    User.findByIdAndUpdate(userId);
+    res.send(accounts);
+  } catch (e) {
+    next(e);
+  }
 };
 
-export default {
-  getAccount,
-};
+export { getAccounts };
