6506b7de194a586588f3f03180d9a333e11a46c5
Anton Myrberg <15anmy@stockholmscience.se>
2017-11-25 21:10:29 +0100

    Added socket.io in a very bad way but it's kind of working

--- src/index.js
+++ src/index.js
@@ -4,15 +4,53 @@
  */
 import express from 'express';
 import chalk from 'chalk';
+import http from 'http';
+import socketIO from 'socket.io';
 
 import './config/database';
 import middlewaresConfig from './config/middlewares';
+import Customers from './queueIO/customers';
 import constants from './config/constants';
 import ApiRoutes from './routes';
 
+// import queueController from './controllers/queue.controller';
+
 const app = express();
 
-// Wrap all the middlewares with the server
+const io = socketIO(http.Server(app)).listen(5000);
+
+const customers = new Customers();
+io.on('connection', socket => {
+  console.log('New customer in in the queue');
+  io.emit('test', 'hello');
+
+  socket.on('join', (query, callback) => {
+    socket.join(query.room);
+    socket.addCustomer(socket.id, query.room);
+
+    io
+      .to(query.room)
+      .emit('updateCustomerList', customers.customersCount(query.room));
+    socket.emit('newMessage', customers.customersCount(query.room));
+    callback();
+  });
+
+  socket.on('test', customer => {
+    io.to(customer).emit('test', 'hello');
+  });
+
+  socket.on('resolveCustomer', () => {
+    const customer = customers.resolveCustomer(socket.id);
+
+    if (customer) {
+      io
+        .to(customer.room)
+        .emit('updateCustomerCount', customers.customersCount(customer.room));
+    }
+  });
+});
+
+// Wrap the app in the middlewares
 middlewaresConfig(app);
 
 // Add the apiRoutes stack to the server
