134bbe48cbd7b9dc102f591dc141108299f59541
Anton Myrberg <15anmy@stockholmscience.se>
2017-11-25 13:26:41 +0100

    Added some sample data

--- src/services/account-info.js
+++ src/services/account-info.js
@@ -4,4 +4,23 @@
   TODO:
     use nordea-api.js service to create api calls
  */
-export const fetchAccounts = () => {};
+export const fetchAccounts = () => {
+  const accounts = [
+    {
+      accountName: 'savings',
+      balance: '1233-EUR',
+    },
+    {
+      accountName: 'savings',
+      balance: '1721-SEK',
+    },
+    {
+      accountName: 'party',
+      balance: '1243-BTC',
+    },
+  ];
+  // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    resolve(accounts);
+  });
+};
