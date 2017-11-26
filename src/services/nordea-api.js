af384b50daf479903bbea7549e219d31eef3c4c0
Anton Myrberg <15anmy@stockholmscience.se>
2017-11-26 02:03:28 +0100

    Added real api calls that may work

--- src/services/nordea-api.js
+++ src/services/nordea-api.js
@@ -1,18 +1,28 @@
 import axios from 'axios';
 
+import constants from '../config/constants';
 /*
   TODO:
     Make the apiCall use the accessToken which is passed from the frontend or not?
     Fix the promise because it's not right
     Uncomment things, it's only for eslint
  */
-export const apiCall = async (route, method) => {
-  const BASIC_URL = 'https://api.hackathon.developer.nordeaopenbanking.com/v2/';
+// eslint-disable-next-line
+export const apiCall = async (method, route, access_token) => {
+  const BASIC_URL = 'https://api.hackathon.developer.nordeaopenbanking.com/v2';
   return new Promise(async (resolve, reject) => {
     switch (method) {
       case 'post':
         try {
-          const body = await axios.post(`${BASIC_URL + route}`);
+          const body = await axios({
+            method: 'post',
+            headers: {
+              access_token,
+              'X-IBM-Client-Id': constants.NORDEA_CLIENT_ID,
+              'X-IBM-Client-Secret': constants.NORDEA_CLIENT_SECRET,
+            },
+            url: `${BASIC_URL + route}`,
+          });
           resolve(body);
         } catch (e) {
           reject(e);
@@ -21,7 +31,15 @@ export const apiCall = async (route, method) => {
 
       case 'get':
         try {
-          const body = await axios.get(`${BASIC_URL + route}`);
+          const body = await axios({
+            method: 'get',
+            headers: {
+              access_token,
+              'X-IBM-Client-Id': constants.NORDEA_CLIENT_ID,
+              'X-IBM-Client-Secret': constants.NORDEA_CLIENT_SECRET,
+            },
+            url: `${BASIC_URL + route}`,
+          });
           resolve(body);
         } catch (e) {
           reject(e);
